3df54f0aeec025c18f735b2717684b5b3fbbb029
Dinh Xuan <xuanyeumy123@gmail.com>
2024-12-10 14:02:42 +0700

    Tính năng account

---
+++ helpers/generate.js
@@ -0,0 +1,11 @@
+module.exports.generateRandomString = (length) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; 
+    const charactersLength = characters.length; 
+    let result = ''; 
+    
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    
+    return result; 
+};
---
+++ models/account.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose'); 
+const generate = require('../heplers/generate.js');
+
+const accountSchema = new mongoose.Schema({
+    fullName: String,
+    email: String,
+    passowrd: String,
+    token: {
+        type: String,
+        default: generate.generateRandomString(20);
+    },
+    phone: String,
+    avatar: String,
+    role_id: String,
+    status: String,
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: Date,
+}, {
+    timestamps: true
+});
+
+
+const Account = new mongoose.model("Account", accountSchema, "accounts");
+
+module.exports = Account;
+
+
