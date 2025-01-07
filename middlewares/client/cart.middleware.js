82f7b5abf1d034c49cb8fdd401e89f0d83801359
Dinh Xuan <xuanyeumy123@gmail.com>
2025-01-07 08:30:51 +0700

    Làm nút miniCart trên Header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -7,12 +7,16 @@ module.exports.cartId = async (req,res,next) => {
         await cart.save();
         
         const expiresTime = 1000 * 60 * 60 * 24 * 365;
-        res.cookie("cartId", cart.id, {
-            expires: new Date(Date.now() + expiresTime)
-        });
+        res.cookie("cartId", cart.id, {expires: new Date(Date.now() + expiresTime)} );
     } 
     else {
+        const cart = await Cart.findOne({
+            _id: req.cookies.cartId
+        });
+        cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0);
 
+
+        res.locals.miniCart = cart;
     }
 
     next();
