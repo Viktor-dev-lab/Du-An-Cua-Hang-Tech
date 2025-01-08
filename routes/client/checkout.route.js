8f6f98683ea54134d89e53f4f821a7ee90c57bf9
Dinh Xuan <xuanyeumy123@gmail.com>
2025-01-08 20:14:58 +0700

    Trang thanh toán thành công

--- routes/client/checkout.route.js
+++ routes/client/checkout.route.js
@@ -4,5 +4,6 @@ const controller = require('../../controllers/client/checkout.controller.js')
 
 route.get('/',controller.index)
 route.post('/order',controller.order)
+route.get('/success/:orderId',controller.success)
 
 module.exports = route
