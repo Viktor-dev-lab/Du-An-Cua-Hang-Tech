7219dcda7e431d9c8e0289bc095780cd4ebb8049
Dinh Xuan <xuanyeumy123@gmail.com>
2025-01-08 14:31:46 +0700

    Cập nhật đặt hàng database

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -31,3 +31,43 @@ module.exports.index = async (req,res) => {
     });
 
 }
+
+// [GET] /checkout/order
+module.exports.order = async (req,res) => {
+    const cartId = req.cookies.cartId;
+    const userInfo = req.body;
+
+    const cart = await Cart.findOne({_id: cartId});
+
+    let products = [];
+
+    for (const product of cart.products) {
+        const objectProduct = {
+            product_id: product.product_id,
+            price: 0,
+            discountPercentage: 0,
+            quantity: product.quantity
+        };
+
+        const productInfo = await Product.findOne({_id: product.product_id});
+        objectProduct.price = productInfo.price;
+        objectProduct.discountPercentage = productInfo.discountPercentage;
+        products.push(objectProduct);
+    }
+    
+    const objectOrder = {
+        cart_id: cartId,
+        userInfo: userInfo,
+        products: products
+    }
+    const order = new Order(objectOrder);
+    await order.save();
+
+    // Reset Cart
+    await Cart.updateOne(
+        {_id: cartId},
+        {products: []}
+    );
+
+    res.redirect(`/checkout/success/${order.id}`);
+}
--- routes/client/checkout.route.js
+++ routes/client/checkout.route.js
@@ -3,5 +3,6 @@ const route = express.Router()
 const controller = require('../../controllers/client/checkout.controller.js')
 
 route.get('/',controller.index)
+route.post('/order',controller.order)
 
 module.exports = route
