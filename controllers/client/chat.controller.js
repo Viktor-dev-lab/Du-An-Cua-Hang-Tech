3794c2edf6f9ec5dd8143bf38f5a422df7efc662
Dinh Xuan <xuanyeumy123@gmail.com>
2025-01-19 11:21:30 +0700

    Làm tin năng chat realtime

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,11 +1,41 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
+
+
 // [GET] /chat
 module.exports.index = async (req, res) => {
+    const userId = res.locals.user.id;
+    const fullName = res.locals.user.fullName;
+
     // Bắt sự kiện connect của socket.io
-    _io.on('connection', (socket) => {
-        console.log('a user connected', socket.id);
+    _io.once('connection', (socket) => {
+        socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+            const chat = new Chat({
+                user_id: userId,
+                content: content
+            });
+            await chat.save();
+            // RealTime Return content to client
+            _io.emit('SERVER_RETURN_MESSAGE', {
+                userId: userId,
+                fullName: fullName,
+                content: content
+            });
+        });
     });
+
+    // Get all of chats
+    const chats = await Chat.find({deleted: false});
+    for (const chat of chats) {
+        const info = await User.findOne({
+            _id: chat.user_id,
+        }).select("fullName");
+
+        chat.info = info;
+    } 
 
     res.render("client/pages/chat/index.pug", {
         pageTitle: "Tin nhắn",
+        chats: chats
     })
 }
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,36 @@
-var socket = io();
+// CLIENT_SEND_MESSAGE
+const formSendData = document.querySelector(".chat .inner-form")
+if (formSendData){
+    formSendData.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const content = e.target.elements.content.value;
+
+        if (content){
+            socket.emit("CLIENT_SEND_MESSAGE", content);
+            e.target.elements.content.value = "";
+        }
+    });
+}
+// CLIENT_SEND_MESSAGE
+
+// SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+    const myId = document.querySelector("[my-id]").getAttribute("my-id");
+    const body = document.querySelector(".chat .inner-body");
+    const div = document.createElement("div");
+
+    let htmlFullName = "";
+    if (myId == data.userId){
+        div.classList.add("inner-outgoing");
+    } else {
+        div.classList.add("inner-incoming");
+        htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
+    }
+
+    div.innerHTML = `
+        ${htmlFullName}
+        <div class="inner-content">${data.content}</div>
+    `
+    body.appendChild(div);
+});
+// SERVER_RETURN_MESSAGE
